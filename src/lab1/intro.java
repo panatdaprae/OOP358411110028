fa56525a9547332ef04d56dda1f76d5ace613ea9
praePanatda <panatda09102539@gmail.com>
2018-11-19 16:34:31 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,21 @@
 package lab1;
 
 public class intro {
-}
+    public static void main(String[] args) {
+        System.out.println("panatda sriphanoen");
+        System.out.println("panatda09102539@gmail.com");
+        System.out.println("MT");
+        System.out.println("RMUTSV");
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+}//class
