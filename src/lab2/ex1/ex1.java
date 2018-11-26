54a10e8311f1b60f29ab4cf519606362efb242bf
praePanatda <panatda09102539@gmail.com>
2018-11-26 14:34:14 +0700

    Lab 2: Variables

---
+++ src/lab2/ex1/ex1.java
@@ -0,0 +1,43 @@
+package lab2.ex1;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+     //integer
+     int a;
+     a=10;
+     System.out.println(a);
+     int b = 20;
+     System.out.println(b);
+     int c=a+b;
+     System.out.println("ผลรวมของ a และ b เท่ากับ\t"+c);
+
+     //real number
+        double p;
+        p=5.5;
+        System.out.println(p);
+        p = p * 10 ;
+        System.out.println(p);
+
+        //character
+        char s = '#';
+        System.out.println(s);
+
+        //string
+        String str = "Panatda Sriphanoen";
+        System.out.println("My name is "+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+}//clss
