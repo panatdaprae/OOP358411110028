2e2747452ce66d02a13491e0a74d0dcafb483def
praePanatda <panatda09102539@gmail.com>
2018-12-15 15:06:25 +0700

    Lab 2: Loop

---
+++ src/lab2/ex1/ex7.java
@@ -0,0 +1,22 @@
+package lab2.ex1;
+
+public class ex7 {
+    public static void main(String[] args) {
+
+        int p = 1;
+        do {
+            System.out.println("Hello."+p);
+        } while (++p <= 10);
+
+
+    }//main
+
+
+
+
+
+
+
+
+
+}//class
