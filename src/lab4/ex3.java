d4217b474be184d550eae600383efd85ced2140d
praePanatda <panatda09102539@gmail.com>
2018-12-17 15:12:22 +0700

    Lap 4: Methods

---
+++ src/lab4/ex3.java
@@ -0,0 +1,19 @@
+package lab4;
+
+public class ex3 {
+    public static void main(String[] args) {
+        A(5.5,5);
+
+    }//main
+   public static int A(int a, int b) {
+        return a * b;
+   }
+   public static double A(double a, double b){
+        return a * b ;
+   }
+
+
+
+
+
+}//class
