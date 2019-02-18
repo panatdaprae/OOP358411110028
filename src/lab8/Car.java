1abc55207b469d2806529cf493a7eae91edb6b3b
praePanatda <panatda09102539@gmail.com>
2019-02-18 13:00:56 +0700

    Lap 8:OOP-Inherit

---
+++ src/lab8/Car.java
@@ -0,0 +1,10 @@
+package lab8;
+
+public interface Car {
+    public void startEngine();
+    public void turnRiht(double x);
+    public void turnLeft(double x);
+    public void stop();
+
+
+}//car
